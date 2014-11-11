7726029af6fb4be03afa6d4a71868c5734b7da26
Sam Corcos <sam@andaril.com>
2014-11-11 09:56:52 -0500

    working on event handlers...

---
+++ client/views/layout.js
@@ -0,0 +1,19 @@
+Template.layout.events({
+  'click .collapsed' : function(event){
+    event.preventDefault();
+    alert("clicked");
+    // $("#main").addClass("dropped");
+  }
+    // $(".collapse").on("click", function() {
+    //   alert("collapsed back");
+    //   // $("#main").removeClass("dropped")
+    // })
+})
+//
+// Template.layout.events({
+//
+//   'click .collapse' : function(event){
+//     event.preventDefault();
+//     alert("also clicked")
+//   }
+// })
