5b123b2cda27edb3f39f82a842e57afd740f252f
Sam Corcos <sam@andaril.com>
2014-11-13 00:29:27 -0500

    solved the collections problem

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ wof_holdings
 #To do
 * Get a list of properties
 * Make sure the list of properties can only be accessed by someone who is logged in
-* Get properties into a collection
+* ~~Get properties into a collection~~
 * Make a unique path for each property
 * Within that unique page, integrate Google Maps and include a carousel of images
---
+++ server/fixtures.js
@@ -0,0 +1,39 @@
+if (Properties.find().count() === 0) {
+
+  Properties.insert({
+    address: "6551 University, Chicago",
+    purchasePrice: "$38,000",
+    rehabCost: "$20,000"
+  });
+
+  Properties.insert({
+      address: "3322 Next Ave, Chicago",
+      purchasePrice: "$120,000",
+      rehabCost: "$50,000"
+  });
+
+  Properties.insert({
+      address: "929 Baltimore St, Chicago",
+      purchasePrice: "$340,000",
+      rehabCost: "$120,000"
+  });
+
+  Properties.insert({
+      address: "6551 University St, Chicago",
+      purchasePrice: "$38,000",
+      rehabCost: "$20,000"
+  });
+
+  Properties.insert({
+      address: "3322 Next Ave, Chicago",
+      purchasePrice: "$120,000",
+      rehabCost: "$50,000"
+  });
+
+  Properties.insert({
+      address: "929 Baltimore St, Chicago",
+      purchasePrice: "$340,000",
+      rehabCost: "$120,000"
+  });
+
+}
